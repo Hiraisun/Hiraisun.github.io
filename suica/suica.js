9c1678413463b3488cb1432d10a66d80151e3ae5
Hiraisun <koushi.2nd@gmail.com>
2023-10-15 18:18:28 +0900

    進化の和など見た目整えた

--- suica/suica.js
+++ suica/suica.js
@@ -1,10 +1,14 @@
 
 //パラメータ類---------------
-const wakuHeight = 600;
+const wakuHeight = 500;
 const wakuWidth = 400;
 
-const canvasElement = document.getElementById("gameCanvas"); //描画位置
+const allHeight = 700
 
+const wakuStart = allHeight - wakuHeight
+const wakuCenter = wakuStart + wakuHeight / 2
+
+const canvasElement = document.getElementById("gameCanvas"); //描画位置
 
 // module aliases
 var Engine = Matter.Engine,
@@ -29,11 +33,13 @@ var render = Render.create({
 	engine: engine,
 	options: {
 		width: wakuWidth,
-		height: wakuHeight,
+		height: allHeight + 50,
 		wireframes: false,
 		background: "rgba(255,255,255,0)",
 	}
 });
+
+
 
 //マウスオブジェクト
 var mouse = Mouse.create(render.canvas),
@@ -58,9 +64,9 @@ wakuParam = {
 	render: { fillStyle: 'rgb(255, 221, 126)', },
 	isStatic: true
 }
-wakuBodys.push(Bodies.rectangle(0, wakuHeight / 2, 40, wakuHeight, wakuParam));
-wakuBodys.push(Bodies.rectangle(wakuWidth / 2, wakuHeight, wakuWidth, 40, wakuParam));
-wakuBodys.push(Bodies.rectangle(wakuWidth, wakuHeight / 2, 40, wakuHeight, wakuParam));
+wakuBodys.push(Bodies.rectangle(0, wakuCenter, 40, wakuHeight, wakuParam));
+wakuBodys.push(Bodies.rectangle(wakuWidth / 2, allHeight, wakuWidth, 20, wakuParam));
+wakuBodys.push(Bodies.rectangle(wakuWidth, wakuCenter, 40, wakuHeight, wakuParam));
 Composite.add(engine.world, wakuBodys);	//ワールドに追加
 
 // run the renderer
@@ -78,148 +84,99 @@ function createCard(num) {
 	//パラメータ
 	var tate = 100
 	var yoko = tate * 1.618
-	var r = 20
-	var renderParam = {
-		sprite: {
-			texture: '../images/twitterQR.png',
-			xScale: 0.5, yScale: 0.5
-		}
-	};
+
+	var rendScale = 0.1;
+	var textureSource = '/suica/card_image/kippu.png';
 
 	switch (num) {	//各カードデータ
 		case 0:
-			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
+			tate = 30
+			yoko = tate * 2
+			rendScale = 0.08;
+			textureSource = '/suica/card_image/kippu.png';
 			break;
 		case 1:
-			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
+			tate = 35
+			yoko = tate * 1.618
+			rendScale = 0.155;
+			textureSource = '/suica/card_image/hayakaken.png';
 			break;
 		case 2:
-			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
+			tate = 40
+			yoko = tate * 1.618
+			rendScale = 0.1;
+			textureSource = '/suica/card_image/kitaca.png';
 			break;
 		case 3:
-			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
+			tate = 45
+			yoko = tate * 1.618
+			rendScale = 0.13;
+			textureSource = '/suica/card_image/toica.png';
 			break;
 		case 4:
 			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
+			yoko = tate * 1.618
+			rendScale = 0.16;
+			textureSource = '/suica/card_image/pitapa.png';
 			break;
 		case 5:
-			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
+			tate = 60
+			yoko = tate * 1.618
+			rendScale = 0.05;
+			textureSource = '/suica/card_image/sugoca.png';
 			break;
 		case 6:
-			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
+			tate = 70
+			yoko = tate * 1.618
+			rendScale = 0.25;
+			textureSource = '/suica/card_image/nimoca.png';
 			break;
 		case 7:
-			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
+			tate = 80
+			yoko = tate * 1.618
+			rendScale = 0.23;
+			textureSource = '/suica/card_image/manaca.png';
 			break;
 		case 8:
-			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
+			tate = 90
+			yoko = tate * 1.618
+			rendScale = 0.63;
+			textureSource = '/suica/card_image/icoca.png';
 			break;
 		case 9:
-			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
+			tate = 100
+			yoko = tate * 1.618
+			rendScale = 0.46;
+			textureSource = '/suica/card_image/pasmo.png';
 			break;
 		case 10:
-			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
-			break;
-		case 11:
-			tate = 50
-			r = 5
-			renderParam.sprite.xScale = 0.1;
-			renderParam.sprite.yScale = 0.1;
-			renderParam.sprite.texture = '../images/twitterQR.png';
+			tate = 120
+			yoko = tate * 1.618
+			rendScale = 0.55;
+			textureSource = '/suica/card_image/suica.png';
 			break;
 		default:
 			break;
 	}
 
-
 	//いざ生成
-	var verti = Bodies.rectangle(0, 0, yoko - r * 2, tate, { render: { visible: false } });
-	var horizon = Bodies.rectangle(0, 0, yoko, tate - r * 2,);
-	var ru = Bodies.circle(yoko / 2 - r, -tate / 2 + r, r,);	//右上
-	var rd = Bodies.circle(yoko / 2 - r, tate / 2 - r, r,);		//右下
-	var ld = Bodies.circle(-yoko / 2 + r, tate / 2 - r, r);		//左下
-	var lu = Bodies.circle(-yoko / 2 + r, -tate / 2 + r, r);	//左上
-	var body2 = Body.create({
-		parts: [verti, horizon, ru, rd, ld, lu],
-		label: "collisionBody",
+	var body = Bodies.rectangle(0, 0, yoko, tate, {
+		label: num,
 		isStatic: true,
+		slop: 0.02,
+		frictionAir: 0.02,
 		collisionFilter: { //オブジェクトに触れられないようにフィルタ
 			mask: 0xFFFFCC,
 		},
-	});
-	//画像用
-	spriteHolder = Bodies.rectangle(0, 0, 200, 200,
-		{
-			collisionFilter: {
-				mask: 0,
-			},
-			render: renderParam,
+		render: {
+			sprite: {
+				texture: textureSource,
+				xScale: rendScale, yScale: rendScale
+			}
 		}
-	)
-	let constraint = Constraint.create({
-		bodyA: body2, bodyB: spriteHolder,
-		pointA: { x: yoko / 2, y: 0 },
-		pointB: { x: yoko / 2, y: 0 },
-		length: 0
 	});
-	let constraint2 = Constraint.create({
-		bodyA: body2, bodyB: spriteHolder,
-		pointA: { x: -yoko / 2, y: 0 },
-		pointB: { x: -yoko / 2, y: 0 },
-		length: 0
-	});
-	let group = Composite.create({ label: `group` });
 
-	Body.setPosition(body2, { x: 300, y: 100 });
-
-	Composite.add(group, [body2, spriteHolder, constraint, constraint2])
-
-
-	return group;
+	return body;
 
 }
 
@@ -229,28 +186,36 @@ var holdingObj = createCard(0);	//id0を生成
 //カード
 Composite.add(engine.world, holdingObj);
 
-
-//マウスクリック時
-Events.on(mouseConstraint, 'mousedown', function (event) {
+//クリック
+canvasElement.addEventListener('click', function () {
 	//持ってるやつを実体化
-	Body.setStatic(holdingObj.bodies[0], false);
-	holdingObj.bodies[0].collisionFilter.mask = 1;
+	Body.setStatic(holdingObj, false);
+	holdingObj.collisionFilter.mask = 1;
 
 	//次のカード生成
-	randnum = Math.floor(Math.random() * 2);	//乱数
+	var randnum = Math.floor(Math.random() * 5);	//乱数
 	holdingObj = createCard(randnum);	//出た目のやつ生成
-	Body.setPosition(holdingObj.bodies[0], { x: mouse.position.x, y: 50 });
+	Body.setPosition(holdingObj, { x: mouse.position.x, y: 50 });
 	//ボール生成
 	Composite.add(engine.world, holdingObj);	//追加
 });
+
+//ホイール
+window.onmousewheel = function () {
+	if (event.wheelDelta > 0) {
+		Matter.Body.rotate(holdingObj, -Math.PI / 16)
+	} else {
+		Matter.Body.rotate(holdingObj, Math.PI / 16)
+	}
+}
+
 
 //tick処理
 Events.on(runner, "beforeTick", function (event) {
 	//var holdMatt = Matter.Composite.get(engine.world, holdingID, "body");
 
 	//マウス追従
-	console.log(holdingObj);
-	Body.setPosition(holdingObj.bodies[0], { x: mouse.position.x, y: 50 });
+	Body.setPosition(holdingObj, { x: mouse.position.x, y: 100 });
 
 });
 
@@ -259,9 +224,9 @@ Events.on(engine, "collisionStart", function (event) {
 	var pairs = event.pairs;
 	pairs.forEach(pair => {
 
-		if (pair.bodyA.label >= 0) {	//数字ラベル(カード)で
+		if (pair.bodyA.label >= 0 && pair.bodyA.label != 10) {	//数字ラベル(カード)で
 			if (pair.bodyA.label == pair.bodyB.label) {	//ラベル一致
-				console.log(pair);
+
 				//合体！
 				//中間に生成
 				newBody = createCard(pair.bodyA.label + 1);	//次のidのやつ
